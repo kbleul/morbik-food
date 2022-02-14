028dddffe795c1a0f118abc2c29823f75ad74ca0
kiBROm <danu@localhost.localdomain>
2022-02-14 08:34:49 -0500

    first commit
    setup tailwind

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from "./header";
+import Footer from "./fotter"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="grid grid-cols-8 gap-3">
+    <div className="col-span-9 border-2 border-gray-500">
+      <Header />
+      </div>
+      <nav className="  col-span-1 row-start-2 row-end-2 border-2 border-gray-500"></nav>
+      <main className="col-start-2 col-end-9 row-start-2 row-end-5 border-2 border-gray-500">
+
+      </main>
+      <div className="col-span-9 border-2 border-gray-500">
+      <Footer />
+      </div>
     </div>
   );
 }
---
+++ src/header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import logo from '././imgs/logo/logo.jpg'
+
+const Header = () => {
+  return (
+      <header>
+        <img src={logo} alt="logo" />
+      </header>
+  )
+};
+
+export default Header;
