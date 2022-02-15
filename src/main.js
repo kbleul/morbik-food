f7c1c03921fe7d907a10c5206b513095b7a110bf
kiBROm <danu@localhost.localdomain>
2022-02-15 07:01:47 -0500

    api setup 1

--- src/main.js
+++ src/main.js
@@ -3,31 +3,83 @@ import axios from 'axios'
 
 const Main = () => {
   const [catagories, set_catagories] = useState([]);
+  const [frontfood_list, set_frontfood_list] = useState([]);
+  const [currentcatagory, set_currentcatagory] = useState("Beef");
+  const [showrecipe_page , set_showrecipe_page] = useState(false);
+  const [recipefor , set_recipefor] = useState("");
+
+
 
   const getCatagories = async () => {
      try {
-    const result = await axios(`https://www.reddit.com/r/TrueFilm/.json`);
-    const result_two = await axios(`www.themealdb.com/api/json/v1/1/list.php?c=list`);
+    const result = await axios(`https://www.themealdb.com/api/json/v1/1/list.php?c=list`);
+    set_catagories(result.data.meals)
 
-
-    console.log("result")
-      console.log( result.data)
-      console.log("result_two")
-      console.log( result_two.data)
-
-    
-     }catch(e) { console.log("error")}
+     }catch(e) { console.log("Fetch catagory failed ! " + e)}
   }
 
-  useEffect(() => {
-    getCatagories();
-    
-  },[])
+  const getFoods_list = async () => {
+    try {
+      const result = await axios(`https://www.themealdb.com/api/json/v1/1/filter.php?c=${currentcatagory}`)
+      set_frontfood_list(result.data.meals)
+    }catch(e) { console.log(`Fetch ${currentcatagory} based foods failed !  ` + e)}
+
+  }
+
+
+  useEffect(() => {  getCatagories();  },[])
+
+  useEffect(() => { getFoods_list(); },[currentcatagory])
+
   return (
     <article>
-        
+       <section>
+        {
+          catagories.map(item => (
+            <button key={item.strCategory} onClick={() => set_currentcatagory(item.strCategory) }>{item.strCategory}</button>
+          ))
+        }
+        </section>
+
+  { showrecipe_page ? <RecipePage foodid={recipefor}/> :
+     <section className="grid grid-cols-3 gap-2">
+         {
+           frontfood_list.map(item => (
+             <div key={item.idMeal} onClick={() => { set_recipefor(item.idMeal); set_showrecipe_page(true);}}>
+             <img key={item.strMealThumb} src={item.strMealThumb} alt={item.strMeal} />
+             <p key={item.strMeal}>{item.strMeal}</p>
+
+             </div>
+           ))
+         }
+        </section>}
     </article>
   )
+}
+
+const RecipePage = ({foodid}) => {
+
+  const [recipedata, set_recipedata] = useState([]);
+
+  const getRecipe = async () => {
+    try {
+      const result = await axios(`www.themealdb.com/api/json/v1/1/lookup.php?i=${foodid}`)
+      set_recipedata(result.data.meals)
+    }catch(e) { console.log(`Fetch recipe for ${foodid} failed !  ` + e)}
+  }
+
+  useEffect(() => {  getRecipe()  },[])
+
+  return (<section>
+    {
+      recipedata.map(item => (
+        <div key={`${item.strMeal} ${item.idMeal}`}>
+          <h4 key={item.strMeal}>{item.strMeal}</h4>
+          <p key={item.strMeal}>{item.strInstructions}</p>
+        </div>
+      ))
+    }
+    </section>)
 }
 
 export default Main
